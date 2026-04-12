eebd38aa3a7ee9182bdd0ac951f41f036d685762
turgayyilmaz34 <turgay.yilmaz@cevalogistics.com>
2026-04-13 02:55:53 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,18 @@
 
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB82otnxaf4Zv4gJpoQjNTq2eJjdJVFgiQ",
+  authDomain: "logio-prod.firebaseapp.com",
+  projectId: "logio-prod",
+  storageBucket: "logio-prod.firebasestorage.app",
+  messagingSenderId: "293378908626",
+  appId: "1:293378908626:web:795e9be2ab7f4e500d280b",
+  measurementId: "G-7G0T8T999Z"
+}
+
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
+export const auth = getAuth(app)
